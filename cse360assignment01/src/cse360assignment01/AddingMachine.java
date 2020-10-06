f5b8d85753839c1e565bbd1026374a7481f87027
Dhruv Koulgi <dkoulgi@asu.edu>
2020-10-05 21:10:10 -0700

    Final commit of AddingMachine with Javadoc comments

--- cse360assignment01/src/cse360assignment01/AddingMachine.java
+++ cse360assignment01/src/cse360assignment01/AddingMachine.java
@@ -5,6 +5,13 @@ public class AddingMachine {
 	  private int total;
 	  private String result = "0";
 	  
+	  /**
+	   * @param total
+	   * the total value of the calculations
+	   * @param result
+	   * the resulting string showing the calculations
+	   */
+	  
 	  public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
 	  }
@@ -13,10 +20,20 @@ public class AddingMachine {
 	    return total;
 	  }
 	  
+	  /**
+	   * @param value
+	   * the number being entered into the calculation
+	   */
+	  
 	  public void add (int value) {
 		  total = total + value;
 		  result += " + " + value;
 	  }
+	  
+	  /**
+	   * @param value
+	   * the number being entered into the calculation
+	   */
 
 	  public void subtract (int value) {
 		  total = total - value;
